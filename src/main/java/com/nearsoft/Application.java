30f69f74a7428c8710efe708f2a6d3d506a7e03b
Omar Bautista <joxebus@gmail.com>
2016-04-20 20:34:38 -0700

    Base Project Structure

---
+++ src/main/java/com/nearsoft/Application.java
@@ -0,0 +1,18 @@
+package com.nearsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot main Application
+ *
+ * @author: Omar Bautista
+ * date: April 20, 2016
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
